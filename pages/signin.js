3cbf715142085818c57694f4b9ac39b7af17e2d5
Manprit <manpritkaur44209@gmail.com>
2021-09-26 16:30:34 +0530

    created sign in page

---
+++ pages/signin.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Component } from 'react';
+import {View , Text} from 'react-native';
+
+export default class SignIn extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Sign In
+                </Text>
+            </View>
+        )
+    }
+}
